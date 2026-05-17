d528175d9618f081444711b0f05c7fede8cbf4d8
ChampMo <97272854+ChampMo@users.noreply.github.com>
2026-05-18 00:19:45 +0700

    build: fix next.js page module compilation error

---
+++ src/app/projects/[id]/page.tsx
@@ -0,0 +1,7 @@
+export default function ProjectDetailPage() {
+  return (
+    <div className="p-10 font-mono text-center text-sky-400">
+      [ STELLAR CONSTELLATION REGISTRY LOG ]
+    </div>
+  );
+}
